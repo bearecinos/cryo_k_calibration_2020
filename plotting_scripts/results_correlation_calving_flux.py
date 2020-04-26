50d860a5b3ea639706fa40e3c595ced450729aa0
bearecinos <recinos@uni-bremen.de>
2020-04-26 13:30:02 +0100

    added and finish improving plots

--- plotting_scripts/results_correlation_calving_flux.py
+++ plotting_scripts/results_correlation_calving_flux.py
@@ -24,6 +24,11 @@ output_dir_path = os.path.join(MAIN_PATH, 'output_data/9_summary_output/')
 df_both = pd.read_csv(os.path.join(output_dir_path,
                                 'glacier_stats_both_methods.csv'))
 
+df_both['diff_k'] = (df_both['k_value_MV'] - df_both['k_value_MR']).abs()
+
+df_both  = df_both.loc[df_both.diff_k != 0]
+df_both = df_both.loc[df_both.k_value_MR !=0]
+
 ## Add total precipitation over the glacier
 df_climate = pd.read_csv(os.path.join(MAIN_PATH,
 'output_data/11_climate_stats/glaciers_PDM_temp_at_the_freeboard_height.csv'),
@@ -37,56 +42,73 @@ df = pd.merge(left=df_both,
               left_on = 'rgi_id',
               right_on='rgi_id')
 
-## Select data to plot
-df  = df.loc[df.k_value_MR != 0]
+# ## Select data to plot
+# df  = df.loc[df.k_value_MR != 0]
 
 # corr_matrix = df.corr(method='pearson')
 # corr_matrix.to_csv(os.path.join(plot_path,
 #                                 'correlation_matrix_all.csv'))
-# exit()
+
 df_vel = df[['k_value_MV',
              'calving_slope',
              'calving_free_board',
              'calving_front_width',
-             'total_prcp_top'
+             'calving_flux_MV',
+             'calving_thick_MV',
+            'calving_water_depth_MV'
              ]].copy()
 
 ## Correlation test's
-corr_vel_k_s, p_vel_k_s = stats.pearsonr(df_vel.k_value_MV,
-                                 df_vel.calving_slope)
-corr_vel_k_f, p_vel_k_f = stats.pearsonr(df_vel.k_value_MV,
+corr_vel_q_k, p_vel_q_k = stats.pearsonr(df_vel.calving_flux_MV,
+                                 df_vel.k_value_MV)
+corr_vel_q_f, p_vel_q_f = stats.pearsonr(df_vel.calving_flux_MV,
                                  df_vel.calving_free_board)
-corr_vel_k_w, p_vel_k_w = stats.pearsonr(df_vel.k_value_MV,
+corr_vel_q_w, p_vel_q_w = stats.pearsonr(df_vel.calving_flux_MV,
                                  df_vel.calving_front_width)
-corr_vel_k_p, p_vel_k_p = stats.pearsonr(df_vel.k_value_MV,
-                                 df_vel.total_prcp_top)
+corr_vel_q_h, p_vel_q_h = stats.pearsonr(df_vel.calving_flux_MV,
+                                 df_vel.calving_thick_MV)
+corr_vel_q_d, p_vel_q_d = stats.pearsonr(df_vel.calving_flux_MV,
+                                 df_vel.calving_water_depth_MV)
 
 df_vel.rename(columns={'k_value_MV': 'k_value'}, inplace=True)
+df_vel.rename(columns={'calving_flux_MV': 'calving_flux'}, inplace=True)
+df_vel.rename(columns={'calving_thick_MV': 'calving_thick'}, inplace=True)
+df_vel.rename(columns={'calving_water_depth_MV': 'calving_depth'}, inplace=True)
 df_vel['Method'] = np.repeat('Velocity', len(df_vel.k_value))
 
 df_racmo = df[['k_value_MR',
                'calving_slope',
                'calving_free_board',
                'calving_front_width',
-               'total_prcp_top'
+               'calving_flux_MR',
+               'calving_thick_MR',
+               'calving_water_depth_MR'
              ]].copy()
 
-corr_racmo_k_s, p_racmo_k_s = stats.pearsonr(df_racmo.k_value_MR,
-                                 df_racmo.calving_slope)
-corr_racmo_k_f, p_racmo_k_f = stats.pearsonr(df_racmo.k_value_MR,
+corr_racmo_q_k, p_racmo_q_k = stats.pearsonr(df_racmo.calving_flux_MR,
+                                 df_racmo.k_value_MR)
+corr_racmo_q_f, p_racmo_q_f = stats.pearsonr(df_racmo.calving_flux_MR,
                                  df_racmo.calving_free_board)
-corr_racmo_k_w, p_racmo_k_w = stats.pearsonr(df_racmo.k_value_MR,
+corr_racmo_q_w, p_racmo_q_w = stats.pearsonr(df_racmo.calving_flux_MR,
                                  df_racmo.calving_front_width)
-corr_racmo_k_p, p_racmo_k_p = stats.pearsonr(df_racmo.k_value_MR,
-                                 df_racmo.total_prcp_top)
+corr_racmo_q_h, p_racmo_q_h = stats.pearsonr(df_racmo.calving_flux_MR,
+                                 df_racmo.calving_thick_MR)
+corr_racmo_q_d, p_racmo_q_d = stats.pearsonr(df_racmo.calving_flux_MR,
+                                 df_racmo.calving_water_depth_MR)
 
 df_racmo.rename(columns={'k_value_MR': 'k_value'}, inplace=True)
+df_racmo.rename(columns={'calving_flux_MR': 'calving_flux'}, inplace=True)
+df_racmo.rename(columns={'calving_thick_MR': 'calving_thick'}, inplace=True)
+df_racmo.rename(columns={'calving_water_depth_MR': 'calving_depth'}, inplace=True)
+
 df_racmo['Method'] = np.repeat('RACMO', len(df_racmo.k_value))
 
-df_racmo['k_diff'] = (df_vel.k_value - df_racmo.k_value).abs()
-df_vel['k_diff'] = (df_vel.k_value - df_racmo.k_value).abs()
+# df_racmo['k_diff'] = (df_vel.k_value - df_racmo.k_value).abs()
+# df_vel['k_diff'] = (df_vel.k_value - df_racmo.k_value).abs()
 
 data_all = pd.concat([df_vel, df_racmo], sort=False)
+
+#data_all = data_all.loc[data_all.calving_flux !=0]
 
 #print(data_all)
 
@@ -114,35 +136,34 @@ fig1 = plt.figure(figsize=(16, 14), constrained_layout=True)
 spec = gridspec.GridSpec(2, 2)
 
 ax0 = plt.subplot(spec[0])
-sns.scatterplot(x='calving_slope', y='k_value', data=data_all, hue='Method',
-                ax=ax0)
-ax0.set_xlabel('calving front slope angle \n [$^{o}$]')
-ax0.set_ylabel('$k$ \n [yr$^{-1}$]')
+sns.scatterplot(x='k_value', y='calving_flux', data=data_all, hue='Method',
+                ax=ax0, palette=color_palette[2:4])
+ax0.set_xlabel('$k$ [yr$^{-1}$]')
+ax0.set_ylabel('$q_{calving}$ [$km^3$yr$^{-1}$]')
 at = AnchoredText('a', prop=dict(size=18), frameon=True, loc=2)
-test1 = AnchoredText('$r_{s}$ = '+ str(np.around(corr_racmo_k_s, decimals=3)) +
-                     '\np-value = ' + str(format(p_racmo_k_s, ".3E")) +
-                     '\n$r_{s}$ = ' + str(np.around(corr_vel_k_s, decimals=3)) +
-                     '\np-value = ' + str(format(p_vel_k_s, ".3E")),
+test1 = AnchoredText('$r_{s}$ = '+ str(np.around(corr_racmo_q_k, decimals=3)) +
+                     '\np-value = ' + str(format(p_racmo_q_k, ".3E")) +
+                     '\n$r_{s}$ = ' + str(np.around(corr_vel_q_k, decimals=3)) +
+                     '\np-value = ' + str(format(p_vel_q_k, ".3E")),
                     prop=dict(size=16),
-                    frameon=True, loc=6,
+                    frameon=True, loc=1,
                     bbox_transform=ax0.transAxes)
 test1.patch.set_boxstyle("round,pad=0.,rounding_size=0.2")
-
 ax0.add_artist(at)
 ax0.add_artist(test1)
 ax0.legend()
 ax0.get_legend().remove()
 
 ax1 = plt.subplot(spec[1])
-sns.scatterplot(x='calving_free_board', y='k_value', data=data_all, hue='Method',
-                ax=ax1)
-ax1.set_xlabel('freeboard \n [m]')
-ax1.set_ylabel('$k$ \n [yr$^{-1}$]')
+sns.scatterplot(x='calving_free_board', y='calving_flux', data=data_all, hue='Method',
+                ax=ax1, palette=color_palette[2:4])
+ax1.set_xlabel('freeboard [m]')
+ax1.set_ylabel('$q_{calving}$ [$km^3$yr$^{-1}$]')
 at = AnchoredText('b', prop=dict(size=18), frameon=True, loc=2)
-test1 = AnchoredText('$r_{s}$ = '+ str(np.around(corr_racmo_k_f, decimals=3)) +
-                     '\np-value = ' + str(format(p_racmo_k_f, ".3E")) +
-                     '\n$r_{s}$ = ' + str(np.around(corr_vel_k_f, decimals=3)) +
-                     '\np-value = ' + str(format(p_vel_k_f, ".3E")),
+test1 = AnchoredText('$r_{s}$ = '+ str(np.around(corr_racmo_q_f, decimals=3)) +
+                     '\np-value = ' + str(format(p_racmo_q_f, ".3E")) +
+                     '\n$r_{s}$ = ' + str(np.around(corr_vel_q_f, decimals=3)) +
+                     '\np-value = ' + str(format(p_vel_q_f, ".3E")),
                     prop=dict(size=16),
                     frameon=True, loc=1,
                     bbox_transform=ax1.transAxes)
@@ -152,15 +173,15 @@ ax1.add_artist(test1)
 ax1.get_legend().remove()
 
 ax2 = plt.subplot(spec[2])
-sns.scatterplot(x='calving_front_width', y='k_value', data=data_all, hue='Method',
-                ax=ax2)
-ax2.set_xlabel('calving front width \n [km]')
-ax2.set_ylabel('$k$ \n [yr$^{-1}$]')
+sns.scatterplot(x='calving_front_width', y='calving_flux', data=data_all, hue='Method',
+                ax=ax2, palette=color_palette[2:4])
+ax2.set_xlabel('calving front width [km]')
+ax2.set_ylabel('$q_{calving}$ [$km^3$yr$^{-1}$]')
 at = AnchoredText('c', prop=dict(size=18), frameon=True, loc=2)
-test1 = AnchoredText('$r_{s}$ = '+ str(np.around(corr_racmo_k_w, decimals=3)) +
-                     '\np-value = ' + str(format(p_racmo_k_w, ".3E")) +
-                     '\n$r_{s}$ = ' + str(np.around(corr_vel_k_w, decimals=3)) +
-                     '\np-value = ' + str(format(p_vel_k_w, ".3E")),
+test1 = AnchoredText('$r_{s}$ = '+ str(np.around(corr_racmo_q_w, decimals=3)) +
+                     '\np-value = ' + str(format(p_racmo_q_w, ".3E")) +
+                     '\n$r_{s}$ = ' + str(np.around(corr_vel_q_w, decimals=3)) +
+                     '\np-value = ' + str(format(p_vel_q_w, ".3E")),
                     prop=dict(size=16),
                     frameon=True, loc=1,
                     bbox_transform=ax2.transAxes)
@@ -170,16 +191,35 @@ ax2.add_artist(test1)
 ax2.get_legend().remove()
 
 ax3 = plt.subplot(spec[3])
-sns.scatterplot(x='total_prcp_top', y='k_value', data=data_all, hue='Method',
-                ax=ax3)
-ax3.set_xlabel('Avg. total solid prcp \n [mm/yr]')
-ax3.set_ylabel('$k$ \n [yr$^{-1}$]')
+# sns.scatterplot(x='calving_thick', y='calving_flux', data=data_all, hue='Method',
+#                 ax=ax3)
+# ax3.set_xlabel('calving front thickness [m]')
+# ax3.set_ylabel('$q_{calving}$ [$km^3$yr$^{-1}$]')
+# ax3.xaxis.set_major_locator(plt.MaxNLocator(3))
+# at = AnchoredText('d', prop=dict(size=18), frameon=True, loc=2)
+# test1 = AnchoredText('$r_{s}$ = '+ str(np.around(corr_racmo_q_h,
+#                     decimals=3)) + '\np-value = ' + str(format(p_racmo_q_h, ".3E")),
+#                     prop=dict(size=16, color=color_palette[2]),
+#                     frameon=True, loc=1,
+#                     bbox_transform=ax3.transAxes)
+# test1.patch.set_boxstyle("round,pad=0.,rounding_size=0.2")
+# test2 = AnchoredText('$r_{s}$ = '+ str(np.around(corr_vel_q_h,
+#                     decimals=3)) + '\np-value = ' + str(format(p_vel_q_h, ".3E")),
+#                     prop=dict(size=16, color=color_palette[3]),
+#                     frameon=True, loc=7,
+#                     bbox_transform=ax3.transAxes)
+# test2.patch.set_boxstyle("round,pad=0.,rounding_size=0.2")
+
+sns.scatterplot(x='calving_depth', y='calving_flux', data=data_all, hue='Method',
+                ax=ax3, palette=color_palette[2:4])
+ax3.set_xlabel('water depth [m]')
+ax3.set_ylabel('$q_{calving}$ [$km^3$yr$^{-1}$]')
 ax3.xaxis.set_major_locator(plt.MaxNLocator(3))
 at = AnchoredText('d', prop=dict(size=18), frameon=True, loc=2)
-test1 = AnchoredText('$r_{s}$ = '+ str(np.around(corr_racmo_k_p, decimals=3)) +
-                     '\np-value = ' + str(format(p_racmo_k_p, ".3E")) +
-                     '\n$r_{s}$ = ' + str(np.around(corr_vel_k_p, decimals=3)) +
-                     '\np-value = ' + str(format(p_vel_k_p, ".3E")),
+test1 = AnchoredText('$r_{s}$ = '+ str(np.around(corr_racmo_q_d, decimals=3)) +
+                     '\np-value = ' + str(format(p_racmo_q_d, ".3E")) +
+                     '\n$r_{s}$ = ' + str(np.around(corr_vel_q_d, decimals=3)) +
+                     '\np-value = ' + str(format(p_vel_q_d, ".3E")),
                     prop=dict(size=16),
                     frameon=True, loc=1,
                     bbox_transform=ax3.transAxes)
@@ -195,9 +235,9 @@ fig1.legend(handles, labels, loc='center', ncol=3, fontsize=18,
 
 plt.tight_layout()
 #plt.show()
-#plt.savefig(os.path.join(plot_path, 'correlation_plot.jpg'),
+# plt.savefig(os.path.join(plot_path, 'correlation_plot_q_calving.jpg'),
 #             bbox_inches='tight')
-plt.savefig(os.path.join(plot_path, 'correlation_plot_nolabel.pdf'),
+plt.savefig(os.path.join(plot_path, 'correlation_plot_q_calving_nolabels.pdf'),
              bbox_inches='tight')
 
 # # Plot Fig 1
--- plotting_scripts/results_correlation_mu_star.py
+++ plotting_scripts/results_correlation_mu_star.py
@@ -24,6 +24,13 @@ output_dir_path = os.path.join(MAIN_PATH, 'output_data/9_summary_output/')
 df_both = pd.read_csv(os.path.join(output_dir_path,
                                 'glacier_stats_both_methods.csv'))
 
+df_both['diff_mu_star'] = (df_both['calving_mu_star_MV'] - df_both['calving_mu_star_MR']).abs()
+
+df_both  = df_both.loc[df_both.diff_mu_star != 0]
+
+df_both['diff_fluxes_MR'] = df_both['calving_flux_MR'] - df_both['calving_law_flux_MR']
+df_both['diff_fluxes_MV'] = df_both['calving_flux_MV'] - df_both['calving_law_flux_MV']
+
 ## Add total precipitation over the glacier
 df_climate = pd.read_csv(os.path.join(MAIN_PATH,
 'output_data/11_climate_stats/glaciers_PDM_temp_at_the_freeboard_height.csv'),
@@ -40,6 +47,10 @@ df = pd.merge(left=df_both,
 ## Select data to plot
 df  = df.loc[df.k_value_MR != 0]
 
+r_pearson_mu, p_pearson_mu = stats.kendalltau(df.calving_mu_star_MR.values,
+        df.calving_mu_star_MV.values)
+
+
 # corr_matrix = df.corr(method='pearson')
 # corr_matrix.to_csv(os.path.join(plot_path,
 #                                 'correlation_matrix_all.csv'))
@@ -48,11 +59,13 @@ df_vel = df[['k_value_MV',
              'calving_slope',
              'calving_free_board',
              'calving_front_width',
-             'calving_mu_star_MV'
+             'calving_mu_star_MV',
+             'diff_fluxes_MV'
              ]].copy()
 
 df_vel.rename(columns={'k_value_MV': 'k_value'}, inplace=True)
 df_vel.rename(columns={'calving_mu_star_MV': 'mu_star'}, inplace=True)
+df_vel.rename(columns={'diff_fluxes_MV': 'diff_fluxes'}, inplace=True)
 
 df_vel['Method'] = np.repeat('Velocity', len(df_vel.k_value))
 
@@ -60,11 +73,13 @@ df_racmo = df[['k_value_MR',
                'calving_slope',
                'calving_free_board',
                'calving_front_width',
-               'calving_mu_star_MR'
+               'calving_mu_star_MR',
+                'diff_fluxes_MR'
              ]].copy()
 
 df_racmo.rename(columns={'k_value_MR': 'k_value'}, inplace=True)
 df_racmo.rename(columns={'calving_mu_star_MR': 'mu_star'}, inplace=True)
+df_racmo.rename(columns={'diff_fluxes_MR': 'diff_fluxes'}, inplace=True)
 
 df_racmo['Method'] = np.repeat('RACMO', len(df_racmo.k_value))
 
@@ -73,27 +88,13 @@ df_vel['k_diff'] = (df_vel.k_value - df_racmo.k_value).abs()
 
 data_all = pd.concat([df_vel, df_racmo], sort=False)
 
-#print(data_all)
-
-
 data_all['calving_front_width'] = data_all.loc[:,'calving_front_width']*1e-3
 
-
-
-# data_all.rename(columns={'k_value': '$k$ \n [$yr^{-1}$]',
-#                         'calving_mu_star': '$\mu^{*}$ \n [mm $yr^{-1}K^{-1}$]',
-#                          'total_prcp_top': 'precipitation \n [mm $yr^{-1}$]',
-#                          'calving_slope': 'slope angle \n [$^\circ$]',
-#                          'calving_free_board': 'free board \n [m a.s.l]',
-#                          'calving_front_width': 'calving front width \n [m]'},
-#                 inplace=True)
-
-#Now plotting
 import matplotlib.gridspec as gridspec
 
-fig1 = plt.figure(figsize=(12, 6))
+fig1 = plt.figure(figsize=(10, 12))
 color_palette = sns.color_palette("muted")
-spec = gridspec.GridSpec(1, 2)
+spec = gridspec.GridSpec(3, 1)
 
 ax0 = plt.subplot(spec[0])
 data = data_all[['mu_star', 'Method']]
@@ -102,22 +103,22 @@ g = sns.FacetGrid(data, hue="Method")
 g = g.map(sns.distplot, "mu_star", bins=bins*0.3, ax=ax0)
 plt.close(2)
 ax0.axvline(linewidth=2, color='k',  linestyle='--')
-ax0.set_xlabel('Temperature sensitivity \n $\mu^{*}$ [mm $yr^{-1}K^{-1}$]')
+ax0.set_xlabel('$\mu^{*}$ [mm $yr^{-1}K^{-1}$]')
 ax0.set_ylabel('Density')
 at = AnchoredText('a', prop=dict(size=18), frameon=True, loc=2)
 ax0.add_artist(at)
 
 ax1 = plt.subplot(spec[1])
 corr, p = stats.pearsonr(data_all.mu_star, data_all.k_value)
-sns.scatterplot(x='mu_star', y='k_value', data=data_all, hue='Method',
+sns.scatterplot(x='k_value', y='mu_star', data=data_all, hue='Method',
                 ax=ax1, alpha=0.5)
-ax1.set_xlabel('Temperature sensitivity \n $\mu^{*}$ [mm $yr^{-1}K^{-1}$]')
-ax1.set_ylabel('$k$ \n [yr$^{-1}$]')
+ax1.set_xlabel('$k$ [yr$^{-1}$]')
+ax1.set_ylabel('$\mu^{*}$ [mm $yr^{-1}K^{-1}$]')
 ax1.xaxis.set_major_locator(plt.MaxNLocator(3))
 at = AnchoredText('b', prop=dict(size=18), frameon=True, loc=2)
 test = AnchoredText('$r_{s}$ = '+ str(np.around(corr,
                     decimals=5)) + '\np-value = ' + str(format(p, ".3E")),
-                    prop=dict(size=18), frameon=True, loc=1,
+                    prop=dict(size=14), frameon=True, loc=1,
                     bbox_transform=ax1.transAxes)
 test.patch.set_boxstyle("round,pad=0.,rounding_size=0.2")
 ax1.add_artist(at)
@@ -127,9 +128,24 @@ ax1.legend(loc=4)
 ax1.get_legend().remove()
 
 handles, labels = ax1.get_legend_handles_labels()
-ax0.legend(handles, labels, loc=1, fontsize=18)
+ax0.legend(handles, labels, loc=1, fontsize=14)
+
+## Density plot to show fabi if mu_star if being clip or not
+ax2 = plt.subplot(spec[2])
+data = data_all[['diff_fluxes', 'Method']]
+_, bins = np.histogram(data["diff_fluxes"])
+g = sns.FacetGrid(data, hue="Method")
+g = g.map(sns.distplot, "diff_fluxes", bins=bins*0.3, ax=ax2)
+plt.close(2)
+ax2.axvline(linewidth=2, color='k',  linestyle='--')
+ax2.set_xlabel('Difference $q$ - $q_{calving}$')
+ax2.set_ylabel('Density')
+at = AnchoredText('c', prop=dict(size=18), frameon=True, loc=2)
+ax2.add_artist(at)
+
+
 
 plt.tight_layout()
 #plt.show()
 plt.savefig(os.path.join(plot_path, 'correlation_plot_mustar.pdf'),
-             bbox_inches='tight')
+              bbox_inches='tight')
